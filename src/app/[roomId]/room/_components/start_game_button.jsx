cc49ea1c887ff74124c6ec46cd82a347e4b5af8b
Junhyoung <junh3951@me.com>
2024-10-06 06:17:09 +0900

    Update start_game_button.jsx

--- src/app/[roomId]/room/_components/start_game_button.jsx
+++ src/app/[roomId]/room/_components/start_game_button.jsx
@@ -12,9 +12,9 @@ export default function StartGameButton() {
 	}
 
 	return (
-		<div className="fixed bottom-0 pb-8 w-full flex justify-center">
+		<div className="fixed bottom-0 pb-12 w-full flex justify-center">
 			<button
-				onClick={StartGameButton}
+				onClick={handleStartGame}
 				className={`button w-3/4 h-16 rounded-lg select-none transition-all duration-150 border-b-[1px] bg-gradient-to-r from-[#C0C0C0] to-[#E0E0E0] cursor-pointer text-[#4A4A4A] [box-shadow:0_10px_0_0_#C0C0C0,0_15px_0_0_#E0E0E0] active:translate-y-2 active:[box-shadow:0_0px_0_0_#C0C0C0,0_0px_0_0_#E0E0E0] active:border-b-[0px] border-[#D0D0D0]`}
 				role="button"
 			>
