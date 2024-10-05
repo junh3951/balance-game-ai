209266fd1863cac2984308293b458637d2a9744c
Junhyoung <junh3951@me.com>
2024-10-06 04:00:18 +0900

    UPDATE: design logo

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import Image from 'next/image'
 import NameInput from './_components/name_input'
 import ActionButtons from './_components/action_buttons'
 import { useRecoilState } from 'recoil'
@@ -17,7 +18,30 @@ export default function HomePage() {
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen p-4">
-			<NameInput name={name} setName={setName} />
+			<div className="flex flex-col items-center justify-center">
+				{/* 이미지 오버랩 효과 */}
+				<div className="relative flex space-x-2 justify-center w-full pb-16">
+					<div className="absolute left-5 z-10">
+						<Image src="/흑.png" alt="흑" width={60} height={60} />
+					</div>
+					<div className="absolute left-16 z-10">
+						<Image src="/백.png" alt="백" width={60} height={60} />
+					</div>
+				</div>
+				<div className="mb-6">
+					<Image
+						src="/논리사.png"
+						alt="논리사"
+						width={160}
+						height={80}
+					/>
+				</div>
+			</div>
+
+			<div className="mb-32">
+				<NameInput name={name} setName={setName} />
+			</div>
+
 			<ActionButtons isActive={name.trim() !== ''} />
 		</div>
 	)
