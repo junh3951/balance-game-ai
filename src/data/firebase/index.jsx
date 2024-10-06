c85b4c14c63fcfefe6a1a34350bf838df6db51d0
Junhyoung <junh3951@me.com>
2024-10-06 13:15:59 +0900

    Update index.jsx

--- src/data/firebase/index.jsx
+++ src/data/firebase/index.jsx
@@ -1,19 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database';
-import { getFirestore} from 'firebase/firestore';
-
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: "balance-ai-fa833.firebaseapp.com",
-    databaseURL: "https://balance-ai-fa833-default-rtdb.firebaseio.com",
-    projectId: "balance-ai-fa833",
-    storageBucket: "balance-ai-fa833.ㄱappspot.com",
-    messagingSenderId: "45387707024",
-    appId: "1:45387707024:web:730a40cf6a776fa0b65e15",
-    measurementId: "G-K0KLDZW7G0"
-  };
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: 'balance-ai-fa833.firebaseapp.com',
+	databaseURL: 'https://balance-ai-fa833-default-rtdb.firebaseio.com',
+	projectId: 'balance-ai-fa833',
+	storageBucket: 'balance-ai-fa833.appspot.com',
+	messagingSenderId: '45387707024',
+	appId: '1:45387707024:web:730a40cf6a776fa0b65e15',
+	measurementId: 'G-K0KLDZW7G0',
+}
 
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
-const firestore = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Initialize Database and Firestore
+export const database = getDatabase(app)
+export const firestore = getFirestore(app)
