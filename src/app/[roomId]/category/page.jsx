b4f6a24ee6da981d43d30e9c5dfcb7e4e664cdf1
Junhyoung <junh3951@me.com>
2024-10-07 19:20:06 +0900

    Update page.jsx

--- src/app/[roomId]/category/page.jsx
+++ src/app/[roomId]/category/page.jsx
@@ -13,6 +13,7 @@ import {
 	trackCategorySelection,
 	determineSelectedCategory,
 	setGameStage,
+	getRoomData, // 추가된 함수
 } from '@/data/api/statemanager'
 
 export default function CategoryPage() {
@@ -24,9 +25,26 @@ export default function CategoryPage() {
 		selectedCount: 0,
 		totalParticipants: 0,
 	})
+	const [roomData, setRoomData] = useState(null) // roomData 추가
 	const router = useRouter()
 	const { roomId } = useParams() // roomId 가져오기
 	const categories = ['순한맛', '중간맛', '매운맛'] // 주제 자극도 설정
+
+	// roomData 가져오기 및 호스트 정보 확인
+	useEffect(() => {
+		const fetchRoomData = async () => {
+			try {
+				const response = await getRoomData(roomId)
+				if (response.status === 200) {
+					setRoomData(response.roomData)
+				}
+			} catch (error) {
+				console.error('Error fetching room data:', error)
+			}
+		}
+
+		fetchRoomData()
+	}, [roomId])
 
 	useEffect(() => {
 		// 선택된 카테고리 초기화
@@ -84,15 +102,17 @@ export default function CategoryPage() {
 				<p className="flex items-center text-bold">
 					{progress.selectedCount} / {progress.totalParticipants} 명이
 					선택했습니다
-					{/* Skip button */}
-					<button
-						onClick={handleSkip}
-						className="ml-4 mb-2 w-auto h-auto px-4 py-1 rounded-full select-none transition-all duration-150 border-b-[1px] bg-gradient-to-r from-[#4A4A4A] to-[#6A6A6A] cursor-pointer text-white [box-shadow:0_10px_0_0_#4A4A4A,0_15px_0_0_#6A6A6A] active:translate-y-2 active:[box-shadow:0_0px_0_0_#4A4A4A,0_0px_0_0_#6A6A6A] active:border-b-[0px] border-[#5A5A5A] hover:bg-gradient-to-r hover:from-[#3A3A3A] hover:to-[#5A5A5A] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-					>
-						<span className="flex flex-col justify-center items-center font-bold text-sm">
-							►
-						</span>
-					</button>
+					{/* 호스트만 스킵 버튼을 볼 수 있게 함 */}
+					{roomData?.hostName === userName && (
+						<button
+							onClick={handleSkip}
+							className="ml-4 mb-2 w-auto h-auto px-4 py-1 rounded-full select-none transition-all duration-150 border-b-[1px] bg-gradient-to-r from-[#4A4A4A] to-[#6A6A6A] cursor-pointer text-white [box-shadow:0_10px_0_0_#4A4A4A,0_15px_0_0_#6A6A6A] active:translate-y-2 active:[box-shadow:0_0px_0_0_#4A4A4A,0_0px_0_0_#6A6A6A] active:border-b-[0px] border-[#5A5A5A] hover:bg-gradient-to-r hover:from-[#3A3A3A] hover:to-[#5A5A5A] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+						>
+							<span className="flex flex-col justify-center items-center font-bold text-sm">
+								►
+							</span>
+						</button>
+					)}
 				</p>
 			</div>
 			<div className="mt-20" />
